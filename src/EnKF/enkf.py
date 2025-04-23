update EnKF + main.py: ensemble computation fixed

src/EnKF/enkf.py
@@ -34,14 +34,24 @@ class EnsembleKalmanFilter:
 
         self.K = np.zeros((xdim, zdim), dtype = np.float32) # Kalman gain matrix
         self.S = np.zeros((zdim, zdim), dtype = np.float32) # Innovation covariance
-
-        self.ensemble_x = multivariate_normal(mean = x.ravel(), cov = self.P, size = N).T # (xdim, N)
+        
+        self.ensemble_x = self.compute_diag_mvn_sample(x.reshape(-1,1), diag_cov=self.P, size = N)
+        # self.ensemble_x = x.reshape(-1,1).repeat(N, axis = 1)    
         self.ensemble_z = np.zeros((zdim, N), dtype = np.float32)            # (zdim, N)
 
         self._mean_x = np.zeros(xdim)
         self._mean_z = np.zeros(zdim)
+        
+    def compute_diag_mvn_sample(self, mean:np.ndarray, diag_cov:np.ndarray, size:int):
+        std_dev = np.sqrt(diag_cov)
+        z = np.random.randn(mean.shape[0], size)
+        return mean + std_dev @ z
 
-    def update(self, z:np.ndarray):
+    def update(self, z:Optional[np.ndarray]):
+        
+        if z is None:
+            self.z = None
+            return
 
         for idx in range(self.N):
             self.ensemble_z[:,idx] = (self.H @ self.ensemble_x[:,idx].reshape(-1,1)).ravel()
@@ -51,27 +61,27 @@ class EnsembleKalmanFilter:
 
         A = self.ensemble_x - self.x.reshape(-1,1)
         HA = self.ensemble_z - z_mean.reshape(-1,1)
-
+        
         self.Pzz = 1 / (self.N - 1) * HA@HA.T + self.R
         self.Pxz = 1 / (self.N - 1) * A@HA.T
 
         self.S = self.Pzz
         self.K = self.Pxz @ np.linalg.inv(self.S)
 
-        err_z = multivariate_normal(mean =self._mean_z, cov = self.R, size=self.N).T
+        err_z = self.compute_diag_mvn_sample(self._mean_z.reshape(-1,1), self.R, self.N)
         
         D = np.concatenate([self.z for _ in range(self.N)], axis = 1) + err_z
         Xp = self.ensemble_x + self.K@(D - self.ensemble_z)
-
+        
         self.x = np.mean(Xp, axis = 1)
         self.P = self.P - self.K@self.S@self.K.T
 
     def predict(self):
-        err_x = multivariate_normal(mean=self._mean_x, cov=self.Q, size=self.N).T
+        err_x = self.compute_diag_mvn_sample(self._mean_x.reshape(-1,1), self.Q, self.N)
 
         for idx in range(self.N):
             self.ensemble_x[:,idx] = self.fx(self.ensemble_x[:,idx]).ravel()
-
+            
         self.ensemble_x += err_x
         self.x = np.mean(self.ensemble_x, axis = 1, keepdims=True)
         
@@ -94,7 +104,7 @@ if __name__ == "__main__":
     R = np.eye(1) * sig_z**2
     B = np.array([0.5 * dt**2, dt]).reshape(-1, 1)
 
-    x0 = np.array([0, 4]).reshape(-1, 1)
+    x0 = np.array([0.0, 4.0]).reshape(-1, 1)
     y_true = 0
 
     traj_estimate = []
@@ -132,4 +142,4 @@ if __name__ == "__main__":
     plt.plot(measurements, label="Measurements")
     plt.plot(predictions[:, 0], label="EnKF Prediction")
     plt.legend()
-    plt.savefig("./result/test_enkf.png")
+    plt.savefig("./results/test_enkf.png")

src/EnKF/util.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def generate_observation_matrix(n_grid:int, n_obs_point:int):
+def generate_observation_matrix(n_grid:int, n_obs_point:int, return_all:bool = False):
     H = np.zeros((n_obs_point, n_grid * n_grid))
     
     indice = [i for i in range(n_grid * n_grid)]
@@ -8,5 +8,20 @@ def generate_observation_matrix(n_grid:int, n_obs_point:int):
     
     for idx, obs_idx in enumerate(obs_indice):
         H[idx, obs_idx] = 1.0
+        
+    if return_all:
+        return H, obs_indice
+    else:
+        return H
+
+def compute_l2_norm(quantity:np.ndarray, quantity_filtered:np.ndarray):
+    l2_norm = np.linalg.norm(quantity - quantity_filtered)
+    return l2_norm
+
+def generate_observation_points(obs_indices:np.ndarray, X_mesh:np.ndarray, Y_mesh:np.ndarray):
+    X_mesh_1D = X_mesh.ravel()
+    Y_mesh_1D = Y_mesh.ravel()
     
-    return H
+    x_obs = X_mesh_1D[obs_indices]
+    y_obs = Y_mesh_1D[obs_indices]
+    return x_obs, y_obs

src/env/sim.py
@@ -10,7 +10,7 @@ from src.env.compute import (
     compute_curl_z, 
     compute_div
 )
-from src.env.slope import slopelimit
+from src.env.slope import slopelimit, smoothing
 from src.env.util import plot_contourf, generate_contourf_gif
 
 class Simulation:
@@ -26,7 +26,8 @@ class Simulation:
         savedir: Optional[str] = "./results/",
         plot_freq: Optional[int] = 16,
         courant_factor: float = 0.5,
-        plot_all:bool = False
+        plot_all:bool = False,
+        use_smooth:bool = False,
     ):
         # Grid configuration
         self.nx = nx
@@ -41,6 +42,7 @@ class Simulation:
         # Computational stability
         self.courant_factor = courant_factor
         self.slopelimit = slopelimit
+        self.use_smooth = use_smooth
 
         # Figure setting
         self.verbose = verbose
@@ -84,7 +86,7 @@ class Simulation:
         self.record = []
 
         self.set_init_condition()
-        
+
     def clear(self):
         self.Et.clear()
         self.KEt.clear()
@@ -234,30 +236,46 @@ class Simulation:
         Bx:Optional[np.ndarray] = None,
         By:Optional[np.ndarray] = None,
         Ez:Optional[np.ndarray] = None,
+        dt:Optional[float] = None,
+        update_params:bool = False,
         ):
 
         # Primitive variables
         if rho is None:
             rho = self.rho
-        
+
         if vx is None:
             vx = self.vx
-        
+
         if vy is None:
             vy = self.vy
-            
+
         if P is None:
             P = self.P
-        
+
         if Bx is None:
             Bx = self.Bx
-            
+
         if By is None:
             By = self.By
-            
+
         if Ez is None:
             Ez = self.Ez
-        
+
+        # Check invalid values
+        rho = np.maximum(rho, 1e-8)
+        P = np.maximum(P, 1e-8)
+
+        # Smoothing
+        if self.use_smooth:
+            rho = smoothing(rho)
+            vx = smoothing(vx)
+            vy = smoothing(vy)
+            P = smoothing(P)
+            Bx = smoothing(Bx)
+            By = smoothing(By)
+            Ez = smoothing(Ez)
+
         bx = self.Bxh
         by = self.Byh
 
@@ -273,10 +291,11 @@ class Simulation:
         vF = self.compute_fast_magnetosonic_speed(vS, vA)
 
         # time difference based on CFL condition
-        dt = self.courant_factor * np.min(self.dx / (vF + np.sqrt(vx**2 + vy**2)))
+        if dt is None:
+            dt = self.courant_factor * np.min(self.dx / (vF + np.sqrt(vx**2 + vy**2)))
 
-        if dt > self.dt_min:
-            dt = self.dt_min
+            if dt > self.dt_min:
+                dt = self.dt_min
 
         self.dt = dt
 
@@ -329,29 +348,31 @@ class Simulation:
         Ez, bx, by = self.compute_constrained_transport(bx, by, flux_by_x, flux_bx_y, self.dx, dt)
         Bx, By = compute_avg_field(bx, by)
 
-        # update variables
-        self.Ez = Ez
-        self.Bxh = bx
-        self.Byh = by
-
-        self.J = J
-        self.w = w
-        self.Pm = Pm
-        self.px = px
-        self.py = py
-        self.m = m
-        self.en = en
-
-        self.vx = px / rho
-        self.vy = py / rho
-        self.rho = m
-        self.P = (self.gamma - 1) * (en - 0.5 * (px**2 + py**2) / rho - 1 / 8 / np.pi * (Bx**2 + By**2))
-        self.Bx = Bx
-        self.By = By
-
         # check divergence free
         divB = compute_div(Bx, By, self.dx)
-        self.divB = divB
+
+        # update variables
+        if update_params:
+            self.Ez = Ez
+            self.Bxh = bx
+            self.Byh = by
+
+            self.J = J
+            self.w = w
+            self.Pm = Pm
+            self.px = px
+            self.py = py
+            self.m = np.maximum(m, 1e-8)
+            self.en = en
+
+            self.vx = px / rho
+            self.vy = py / rho
+            self.rho = np.maximum(m, 1e-8)
+            self.P = np.maximum((self.gamma - 1) * (en - 0.5 * (px**2 + py**2) / rho - 1 / 8 / np.pi * (Bx**2 + By**2)), 1e-8)
+            self.Bx = Bx
+            self.By = By
+
+            self.divB = divB
 
         return rho, vx, vy, P, Bx, By, Ez
 
@@ -386,6 +407,20 @@ class Simulation:
             px = self.px
             py = self.py
             en = self.en
+
+            # Check invalid values
+            rho = np.maximum(rho, 1e-8)
+            P = np.maximum(P, 1e-8)
+
+            # Smoothing
+            if self.use_smooth:
+                rho = smoothing(rho)
+                vx = smoothing(vx)
+                vy = smoothing(vy)
+                P = smoothing(P)
+                Bx = smoothing(Bx)
+                By = smoothing(By)
+                Ez = smoothing(Ez)
 
             # Compute wave velocities
             vA = self.compute_Alfven_speed(rho, Bx, By)
@@ -457,13 +492,13 @@ class Simulation:
             self.Pm = Pm
             self.px = px
             self.py = py
-            self.m = m
+            self.m = np.maximum(m, 1e-8)
             self.en = en
 
             self.vx = px / rho
             self.vy = py / rho
-            self.rho = m
-            self.P = (self.gamma - 1) * (en - 0.5 * (px**2 + py**2) / rho - 1 / 8 / np.pi * (Bx**2 + By**2))
+            self.rho = np.maximum(m, 1e-8)
+            self.P = np.maximum((self.gamma - 1) * (en - 0.5 * (px**2 + py**2) / rho - 1 / 8 / np.pi * (Bx**2 + By**2)), 1e-8)
             self.Bx = Bx
             self.By = By
 
@@ -487,12 +522,13 @@ class Simulation:
                         t, np.mean(np.abs(divB)), np.mean(self.en), np.mean(P), np.mean(self.rho)
                     )
                 )
-                self.record.append(rho)
+
+            self.record.append(rho)
 
             # break condition
             if t >= self.t_end:
                 break
-        
+
         if self.plot_all:
             self.plot_snapshot(None, t=t)
             self.plot_energy_evolution()